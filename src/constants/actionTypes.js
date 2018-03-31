b963688592e8c60d3f7726622a70f6734fbe1e81
JD <j@mesjohnson.com>
2018-03-30 20:06:36 -0700

    built store out with mock data

--- src/constants/actionTypes.js
+++ src/constants/actionTypes.js
@@ -3,3 +3,4 @@ export const ADD_STORY = 'ADD_STORY';
 export const UPDATE_STORY = 'UPDATE_STORY';
 export const DELETE_STORY = 'DELETE_STORY';
 export const GET_STORIES = 'GET_STORIES';
+export const LOGIN_USER = 'LOGIN_USER';
